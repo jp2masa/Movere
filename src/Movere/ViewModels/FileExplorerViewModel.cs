6565770b8d3f56d51ce778e4f788710a7976c8fd
José Pedro <jpedrolmail@gmail.com>
2019-08-08 23:30:27 +0100

    Code cleanup.

--- src/Movere/ViewModels/FileExplorerViewModel.cs
+++ src/Movere/ViewModels/FileExplorerViewModel.cs
@@ -21,7 +21,9 @@ namespace Movere.ViewModels
 
         private Folder _currentFolder;
 
+#pragma warning disable CS8618 // Non-nullable field is uninitialized.
         public FileExplorerViewModel(bool allowMultipleSelection)
+#pragma warning restore CS8618 // Non-nullable field is uninitialized.
         {
             AddressBar = new FileExplorerAddressBarViewModel();
 
