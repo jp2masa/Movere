ce8913baa6a0d5a5864f23558bdcd8a243869693
José Pedro <jpedrolmail@gmail.com>
2019-09-22 18:48:26 +0100

    Removed custom Tapped event handler in OverlappedItemsPanel.

--- src/Movere/Controls/OverlappedItemsPanel.cs
+++ src/Movere/Controls/OverlappedItemsPanel.cs
@@ -1,41 +1,10 @@
-﻿using System.Windows.Input;
-
-using Avalonia;
+﻿using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
 
 namespace Movere.Controls
 {
     internal class OverlappedItemsPanel : Panel
     {
-        public static readonly DirectProperty<OverlappedItemsPanel, ICommand?> TappedCommandProperty =
-            AvaloniaProperty.RegisterDirect<OverlappedItemsPanel, ICommand?>(
-                nameof(TappedCommand),
-                p => p.TappedCommand,
-                (p, c) => p.TappedCommand = c);
-
-        public static readonly StyledProperty<object?> TappedCommandParameterProperty =
-            AvaloniaProperty.Register<OverlappedItemsPanel, object?>(nameof(TappedCommandParameter));
-
-        private ICommand? _tappedCommand;
-
-        public OverlappedItemsPanel()
-        {
-            Tapped += OnTapped;
-        }
-
-        public ICommand? TappedCommand
-        {
-            get => _tappedCommand;
-            set => SetAndRaise(TappedCommandProperty, ref _tappedCommand, value);
-        }
-
-        public object? TappedCommandParameter
-        {
-            get => GetValue(TappedCommandParameterProperty);
-            set => SetValue(TappedCommandParameterProperty, value);
-        }
-
         protected override Size MeasureOverride(Size availableSize)
         {
             var size = Size.Empty;
@@ -69,19 +38,6 @@ namespace Movere.Controls
             }
 
             return finalSize;
-        }
-
-        protected void OnTapped(object sender, RoutedEventArgs e)
-        {
-            if (TappedCommand != null)
-            {
-                var parameter = TappedCommandParameter;
-
-                if (TappedCommand.CanExecute(parameter))
-                {
-                    TappedCommand.Execute(parameter);
-                }
-            }
         }
     }
 }
