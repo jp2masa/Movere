36d5bf2c7eb599b0db83d06f7e8eec2334452a23
José Pedro <jpedrolmail@gmail.com>
2019-10-05 18:28:18 +0100

    Added support for IsCancel.

---
+++ src/Movere/Converters/DialogResultIsCancelConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+using Avalonia.Data.Converters;
+
+using Movere.Models;
+
+namespace Movere.Converters
+{
+    internal sealed class DialogResultIsCancelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DialogResult result && targetType == typeof(bool))
+            {
+                return result == DialogResult.Cancel;
+            }
+
+            throw new NotSupportedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
