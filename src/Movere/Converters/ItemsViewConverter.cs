f49571ade0b69aabe294f17530356a8550c747e7
José Pedro <jpedrolmail@gmail.com>
2020-09-16 22:55:52 +0100

    Code cleanup.

--- src/Movere/Converters/ItemsViewConverter.cs
+++ src/Movere/Converters/ItemsViewConverter.cs
@@ -11,7 +11,7 @@ namespace Movere.Converters
 {
     internal sealed class ItemsViewConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is ItemsView view && targetType == typeof(ItemsControlView) && parameter is ResourceDictionary resources)
             {
