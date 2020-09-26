73359bebbd26b75493bc0ff080d32643eb9a29fb
José Pedro <jpedrolmail@gmail.com>
2020-09-26 23:24:43 +0100

    Added content dialog.

---
+++ src/Movere/ViewModels/ContentDialogViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+using ReactiveUI;
+
+using Movere.Models;
+using Movere.Services;
+
+namespace Movere.ViewModels
+{
+    public sealed class ContentDialogViewModel : ReactiveObject
+    {
+        private readonly IDialogView<DialogResult> _view;
+
+        internal ContentDialogViewModel(
+            IDialogView<DialogResult> view,
+            string title,
+            object content,
+            IReadOnlyList<DialogResult> dialogResults)
+        {
+            _view = view;
+
+            Title = title;
+            Content = content;
+
+            DialogResults = dialogResults;
+        }
+
+        public string Title { get; }
+
+        public object Content { get; }
+
+        public IReadOnlyList<DialogResult> DialogResults { get; }
+    }
+}
---
+++ src/Movere/Views/ContentDialog.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+
+using Movere.ViewModels;
+
+namespace Movere.Views
+{
+    internal sealed class ContentDialog : ReactiveWindow<ContentDialogViewModel>
+    {
+        public ContentDialog()
+        {
+            InitializeComponent();
+#if AVALONIA_DIAGNOSTICS
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+    }
+}
